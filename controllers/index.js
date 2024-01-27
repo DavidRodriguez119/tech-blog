a70481d07758677cf0693e4c76410bf4ea15d213
David Rodriguez <santiago1.dsrr@gmail.com>
2024-01-27 17:24:19 -0500

    basic handlebars structure

--- controllers/index.js
+++ controllers/index.js
@@ -11,4 +11,35 @@ router.use(`/api/post`, postRoutes);
 const commentRoutes = require(`./commentRoutes`);
 router.use(`/api/comment`, commentRoutes);
 
+// other routes
+
+// Home page
+router.get(`/`, (req, res) => {
+    const isLoggedIn = req.session.user !== undefined;
+    console.log(isLoggedIn)
+    res.render("home", { isLoggedIn });
+    console.log("Homepage")
+});
+
+// Dashboard pageBreakAfter: 
+router.get(`/dashboard`, (req, res) => {
+    res.render("dashboard");
+});
+
+// Login Page 
+router.get("/login", (req, res) => {
+    if (req.session.user) {
+      res.redirect("/")
+      console.log("homepage")
+    } else {
+      res.render("login")
+      console.log("Login")
+    }
+})
+
+// Logout Page 
+router.get(`/logout`, (req, res) => {
+    res.render("logout");
+});
+
 module.exports = router;
---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Your code here
+    console.log("Page has started");
+});
